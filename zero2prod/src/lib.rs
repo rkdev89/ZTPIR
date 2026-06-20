00bf5a64d2504cd09a20606941055319876839f5
R <richardkemp35@gmail.com>
2026-06-20 13:38:12 +0100

    More integration tests

--- zero2prod/src/lib.rs
+++ zero2prod/src/lib.rs
@@ -1,16 +1,28 @@
 use actix_web::{web,App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,6 +1,8 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    run(listener)?.await
 }
